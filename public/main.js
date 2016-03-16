059a46d172c07831b6ecff76fc4d41b96f0e3412
Patrick Dorante <patchouko1@gmail.com>
2016-03-15 21:18:20 -0400

    ADD A TIMER ON CANVAS

--- public/main.js
+++ public/main.js
@@ -140,12 +140,15 @@ $(function() {
                 game.physics.arcade.collide(player, playerStorage[p].player);
             }
         }
+        
 
         function render() {
             //game.debug.body(player);
             //for (var p in playerStorage) {
             //    game.debug.body(playerStorage[p].player);
             //}
+             game.debug.text('Elapsed seconds: ' + this.game.time.totalElapsedSeconds(), 32, 32);
+
         }
     }
 
